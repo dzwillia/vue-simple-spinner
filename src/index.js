a2aeb959c72d65039e7eecf44804718c02d7d64d
David Z. Williams <dave@flex.io>
2017-04-25 00:03:49 -0400

    Added 'vue-simple-spinner' to global scope.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import VueSimpleSpinner from './components/Spinner.vue'
 
+// expose component to global scope
+if (typeof window !== 'undefined' && window.Vue) {
+  Vue.component('vue-simple-spinner', VueSimpleSpinner)
+}
+
 export default VueSimpleSpinner
 
 export { VueSimpleSpinner }
