49de5bdee73341784fe03eff17bbe0688d0988a4
David Z. Williams <dave@flex.io>
2017-08-04 16:02:30 -0400

    Simpler styling again for examples.

--- examples-src/App.vue
+++ examples-src/App.vue
@@ -8,7 +8,7 @@
         <a href="https://github.com/dzwillia/vue-simple-spinner" rel="noopener" target="_blank" title="View on Github"><svg xmlns="http://www.w3.org/2000/svg" fill="white" width="32" height="32" viewBox="0 0 16 16"><path d="M8 .198c-4.418 0-8 3.582-8 8 0 3.535 2.292 6.533 5.47 7.59.4.075.548-.173.548-.384 0-.19-.008-.82-.01-1.49-2.227.485-2.696-.943-2.696-.943-.364-.924-.888-1.17-.888-1.17-.726-.497.055-.486.055-.486.802.056 1.225.824 1.225.824.714 1.223 1.872.87 2.328.665.072-.517.28-.87.508-1.07-1.776-.202-3.644-.888-3.644-3.954 0-.874.313-1.588.824-2.148-.083-.202-.357-1.015.077-2.117 0 0 .672-.215 2.2.82.64-.177 1.323-.266 2.003-.27.68.004 1.365.093 2.004.27 1.527-1.035 2.198-.82 2.198-.82.435 1.102.162 1.916.08 2.117.512.56.822 1.274.822 2.147 0 3.072-1.872 3.748-3.653 3.946.288.248.544.735.544 1.48 0 1.07-.01 1.933-.01 2.196 0 .213.145.462.55.384 3.178-1.06 5.467-4.057 5.467-7.59 0-4.418-3.58-8-8-8z"></path></svg></a>
       </div>
     </div>
-    <div class="flex-fill" style="padding-bottom: 160px">
+    <div class="flex-fill" style="padding-bottom: 10rem">
       <!-- allow a 3rem padding when resizing browser horizontally and approaching Tachyon's large media breakpoint (60em) -->
       <div class="mb5 center" style="max-width: 57rem">
 
@@ -260,10 +260,10 @@
         return version
       },
       header_cls() {
-        return 'db mt5 pv3 ph3 ph0-l bb b--mid-gray b--black-10 br--top br2 mid-gray hover-blue no-underline'
+        return 'db mt5 pv2 ph3 ph0-l mid-gray hover-blue no-underline'
       },
       box_cls() {
-        return 'db ph3 pt3 pb4 bl-l br-l bb b--black-10 relative center css-box'
+        return 'db pt2 pb5 ph3 ph0-l bt b--black-10 relative'
       },
       box_style() {
         return 'min-height: 100px'
