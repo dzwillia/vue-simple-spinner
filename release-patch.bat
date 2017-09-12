cd972002dfcf4ff4a770a2e81ed65beaafe4c118
David Z. Williams <dave@flex.io>
2017-09-12 00:01:37 -0400

    Update release batch files.

--- release-patch.bat
+++ release-patch.bat
@@ -1,5 +1,5 @@
 call npm version patch
 call yarn
-call yarn run build:all
+call yarn run build
 call git add package.json
 echo **** Don't forget to update the `gh-pages` branch and run `npm publish` ****
