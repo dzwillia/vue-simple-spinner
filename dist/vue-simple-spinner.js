46aaabb955889fbe7717d5f01d45e5b227ea0825
David Z. Williams <dave@flex.io>
2017-09-16 15:10:18 -0400

    examples.bundle.js no longer includes package.json info.

--- dist/vue-simple-spinner.js
+++ dist/vue-simple-spinner.js
@@ -9,9 +9,9 @@
 	else if(typeof define === 'function' && define.amd)
 		define([], factory);
 	else if(typeof exports === 'object')
-		exports["vue-simple-spinner"] = factory();
+		exports["VueSimpleSpinner"] = factory();
 	else
-		root["vue-simple-spinner"] = factory();
+		root["VueSimpleSpinner"] = factory();
 })(this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
